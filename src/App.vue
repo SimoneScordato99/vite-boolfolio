8d65240534d06e2832b75aa0005c29321d377fa1
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-28 18:12:56 +0200

    stampa card

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,6 @@ export default{
 <style lang="scss">
 @use './style/main.scss';
 
-  .webApp{
-    
-  }
+
 
 </style>
--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -1,5 +1,7 @@
 <script >
 import {store} from '../store';
+import axios from 'axios';
+
 
 export default {
     name:'MainVue',
@@ -8,14 +10,40 @@ export default {
     },
     data(){
         return{
-            store
+            store,
+            project:[],
         }
     },
+    mounted(){
+        this.chiamataTutto()
+    },
+    methods:{
+        chiamataTutto(){
+            axios.get(`http://127.0.0.1:8000/api/projects`)
+            .then((res)=>{
+              this.project = res.data.results
+            })
+        }
+    }
 }
 </script>
 
 <template>
-    
+    auiofbvkjasbrkgahrf
+    <div class="container">
+        <div class="row">
+            <div class="col-2" v-for="(elem, index) in project" :key="index">
+                <div class="card" style="width: 18rem;">
+                    <img :src="`http://127.0.0.1:8000/storage/${elem.img}`" class="card-img-top" :alt="`${elem.title}`">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ elem.title }}</h5>
+                        <p class="card-text">{{ elem.description }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
