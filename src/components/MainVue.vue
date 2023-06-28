bd78abe62122f371006827c4783f846f5740830a
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-28 18:56:46 +0200

    VFOR CARDS $ VFOR  LISTA

--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -12,6 +12,8 @@ export default {
         return{
             store,
             project:[],
+            lenguages:[],
+            generes:[]
         }
     },
     mounted(){
@@ -22,6 +24,8 @@ export default {
             axios.get(`http://127.0.0.1:8000/api/projects`)
             .then((res)=>{
               this.project = res.data.results
+              this.lenguages = res.data.results.lenguages
+              this.genere = res.data.results.genere
             })
         }
     }
@@ -32,13 +36,17 @@ export default {
     auiofbvkjasbrkgahrf
     <div class="container">
         <div class="row">
-            <div class="col-2" v-for="(elem, index) in project" :key="index">
-                <div class="card" style="width: 18rem;">
+            <div class="col-6" v-for="(elem, index) in project" :key="index">
+                <div class="card">
                     <img :src="`http://127.0.0.1:8000/storage/${elem.img}`" class="card-img-top" :alt="`${elem.title}`">
                     <div class="card-body">
                         <h5 class="card-title">{{ elem.title }}</h5>
                         <p class="card-text">{{ elem.description }}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text text-success">{{ elem.genere.name }}</p>
+                        <ul class="list-group my-3" v-for="(eleme, index) in elem.lenguages" :key="index">
+                            <li class="list-group-item" >{{ eleme.name }}</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Approfondisci</a>
                     </div>
                 </div>
             </div>
